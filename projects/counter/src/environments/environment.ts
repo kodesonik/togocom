86e9e11d4bf4faca22044bdb767fcee097d2c4db
Kode Sonik <k0d3.s0n1k@gmail.com>
2022-12-25 20:00:29 +0000

    add firebase to counter

---
+++ projects/counter/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'togo-com',
+    appId: '1:383453340562:web:06b6f5eb700ac06483d74b',
+    storageBucket: 'togo-com.appspot.com',
+    apiKey: 'AIzaSyAsKHvrZrlinIIGjBmWgXRhlzBykZjTjbc',
+    authDomain: 'togo-com.firebaseapp.com',
+    messagingSenderId: '383453340562',
+    measurementId: 'G-1Y4XZ0Y8DD',
+  },
+    production: false,
+  };
